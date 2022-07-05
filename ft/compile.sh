4d8ee5c8e1147d60d4ed941a72dabefb6c850d72
mrmld <die.marmelade@yandex.ru>
2022-07-05 20:43:20 +0300

    wip: code

--- ft/compile.sh
+++ ft/compile.sh
@@ -1,6 +1,7 @@
 func -SPA -o ./build/jetton-wallet.fif ../stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-wallet.fc
 echo '"build/jetton-wallet.fif" include 2 boc+>B "build/jetton-wallet.boc" B>file' | fift -s
 func -SPA -o ./build/jetton-minter.fif ../stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter.fc
+echo '"build/jetton-minter.fif" include 2 boc+>B "build/jetton-minter.boc" B>file' | fift -s
 func -SPA -o ./build/jetton-minter-ICO.fif ../stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter-ICO.fc
 
 fift -s build/print-hex.fif
